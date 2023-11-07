50533c06a67afe8e223c0e9c6b3bd0f9e2e8282f
Ansh srivastava <anshs1939@gmail.com>
2023-11-07 23:39:32 +0530

    optimize this code

--- Heap/KthLargestNumber.java
+++ Heap/KthLargestNumber.java
@@ -31,7 +31,11 @@ public class KthLargestNumber {
     }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the alue of n");
+
         int n = sc.nextInt();
+        System.out.println("Enter the alue of k");
+
         int k = sc.nextInt();
         int[] arr = new int[n];
         for(int i=0;i<n;i++){

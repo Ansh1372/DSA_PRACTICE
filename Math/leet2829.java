2c4627d62a1f0d23039e78e2fe8c28edc879c8bc
Ansh srivastava <anshs1939@gmail.com>
2023-11-08 23:27:05 +0530

    add leetcode question 2829 based on math

---
+++ Math/leet2829.java
@@ -0,0 +1,12 @@
+package Math;
+
+public class leet2829 {
+    public static boolean isReachableAtTime(int sx, int sy, int fx, int fy, int t) {
+        int minStep = Math.max(Math.abs(sx - fx), Math.abs(sy - fy));
+        return minStep == 0 ? t != 1 : minStep <= t;
+    }
+    public static void main(String[] args) {
+        System.out.println("true".equals(isReachableAtTime(3,5,-4,7,16)));
+        System.out.println("false".equals(isReachableAtTime(-1,0,0,1,2)));
+    }
+}

fa3f9548d9eb4acedbcac2083b9225711bcf3563
Ansh srivastava <anshs1939@gmail.com>
2023-11-14 23:46:54 +0530

    use static keyword/add static keyword in palindrom question in string

--- Strings/PalindromeString.java
+++ Strings/PalindromeString.java
@@ -2,7 +2,7 @@ package Strings;
 import java.util.*;
 
 public class PalindromeString {
-    public int ispalindrome(String s){
+    public static int ispalindrome(String s){
         int i=0;
         int j=s.length()-1;
         while (i<j) {
@@ -21,8 +21,7 @@ public class PalindromeString {
         System.out.println("Enter a string: ");
     
         String str = sc.nextLine();
-        PalindromeString ps = new PalindromeString();
-        if(ps.ispalindrome(str) == 1){
+        if(ispalindrome(str) == 1){
             System.out.println("The given string is palindrome");
         }
         else{

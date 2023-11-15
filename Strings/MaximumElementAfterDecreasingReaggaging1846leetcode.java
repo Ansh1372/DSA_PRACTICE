f265ce684c47adc9fd8fedfd1950b1658b93a83f
Ansh srivastava <anshs1939@gmail.com>
2023-11-15 22:28:55 +0530

    1846 leetcode mediam level solution

---
+++ Strings/MaximumElementAfterDecreasingReaggaging1846leetcode.java
@@ -0,0 +1,31 @@
+package Strings;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class MaximumElementAfterDecreasingReaggaging1846leetcode {
+     public static int maximumElementAfterDecrementingAndRearranging(int[] arr) {
+        Arrays.sort(arr);
+        int pre = 0;
+        for (int a: arr)
+            pre = Math.min(pre + 1, a);
+            return pre;
+        
+        
+        
+         
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int size = sc.nextInt();
+        int[] arr = new int[size];
+        for(int i=0;i<n;i++){
+            arr[i] = sc.nextInt();
+        }
+        int m = maximumElementAfterDecrementingAndRearranging(arr);
+        System.out.println("Maximum element after decreasing and rearranging is "+m);
+
+
+
+    }
+}

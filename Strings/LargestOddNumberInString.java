7a680898b7b8d434bddbbb4bd858eec81d4c8430
[ansh] <[anshs1939@gmail.com]>
2023-12-07 11:24:36 +0530

    largest odd number in string day 61 challenge in leetcode

---
+++ Strings/LargestOddNumberInString.java
@@ -0,0 +1,22 @@
+package Strings;
+import java.util.*;
+public class LargestOddNumberInString {
+     public static String largestOddNumber(String num) {
+      for(int i=num.length()-1;i>=0;i--){
+          int n=num.charAt(i);
+          if(n%2!=0){
+              return num.substring(0,i+1);
+          }
+      } 
+      return ""; 
+        
+    }
+    public static void main(String[] args) {
+        Scanner d = new Scanner(System.in);
+        System.out.println("Enter the string");
+        String str = d.nextLine();
+        System.out.print("largest odd number in givin string is : ");
+        System.out.print(largestOddNumber(str));
+
+    }
+}

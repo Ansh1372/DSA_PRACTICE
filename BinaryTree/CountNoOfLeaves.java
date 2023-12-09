1534ba257a0a0b24ddb4611f8c2392655d4ac3e9
[ansh] <[anshs1939@gmail.com]>
2023-12-10 03:09:14 +0530

    basic binary tree question

---
+++ BinaryTree/CountNoOfLeaves.java
@@ -0,0 +1,28 @@
+package BinaryTree;
+
+// import org.w3c.dom.Node;
+import java.util.*;
+class Node
+{
+    int data;
+    Node left, right;
+}
+public class CountNoOfLeaves {
+    public static int countLeaves(Node root) 
+    {
+        if(root == null)
+            return 0;
+
+        if(root.left == null && root.right == null)
+            return 1;
+
+        return countLeaves(root.left) + countLeaves(root.right);
+    }
+    // public static void main(String[] args) {
+    //   Node a = new Node();
+    //   a.data = 1;
+
+
+        
+    // }
+}
